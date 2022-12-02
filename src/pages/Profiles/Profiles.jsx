a3a201d89c07ce663702a5ea067381655c3e5ca7
RazorHollow <rob@razorhollow.com>
2022-12-02 14:32:22 -0500

    add fontawesome icons

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -1,5 +1,12 @@
 import { useState, useEffect } from 'react'
 import * as profileService from '../../services/profileService'
+import styles from './Profiles.module.css'
+
+import moment from 'moment'
+import Moment from 'react-moment'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserPlus } from '@fortawesome/pro-solid-svg-icons'
+
 
 const Profiles = () => {
   const [profiles, setProfiles] = useState([])
@@ -12,21 +19,50 @@ const Profiles = () => {
     fetchProfiles()
   }, [])
 
-  console.log(profiles)
-
   return (
-    <>
-      <h1>Hello. This is a list of all the profiles.</h1>
+    <main>
+      <h1>User Index</h1>
+      <div id='addUser'>
+        <FontAwesomeIcon
+          icon={faUserPlus} 
+          size='3x'
+          style={{
+            position: 'fixed',
+            right: '10%',
+            top: '20%'
+          }}
+        />
+      </div>
       {profiles.length ? 
-        <>
+        <table>
+          <thead>
+            <tr>
+              <th>Admin</th>
+              <th>Name</th>
+              <th>Active</th>
+              <th>Date Registered</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody>
           {profiles.map(profile =>
-            <p key={profile._id}>{profile.name} | {profile.isActive ? "true" : "false"}</p>
+            <tr key={profile._id}>
+              <td>{profile.name}</td>
+              <td>{profile.isAdmin ? "Yes" : "No"}</td>
+              <td>{profile.isActive ? "Yes" : "No"}</td>
+              <td><Moment 
+                    date={profile.createdAt}
+                    format="dddd, MMMM Do, YYYY"
+                    /></td>
+              <td><button>Edit User</button></td>
+            </tr>
           )}
-        </>
+          </tbody>
+        </table>
       :
         <p>No profiles yet</p>
       }
-    </>
+    </main>
   )
 }
  
